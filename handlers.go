b886fe9dd9c78446909934fa3d293805b3c6b8bc
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-08-21 23:16:57 +0200

    add GetFeeds to print all the feeds in the db

--- handlers.go
+++ handlers.go
@@ -137,3 +137,15 @@ func handlerAddFeed(s *state, cmd command) error {
 	fmt.Println(feed)
 	return nil
 }
+
+func handlerFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, feed := range feeds {
+		fmt.Println(feed)
+	}
+	return nil
+}
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,9 @@
 INSERT INTO feeds (created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetFeeds :many
+SELECT feeds.name, feeds.url, u.name as username
+FROM feeds
+INNER JOIN users as u
+ON u.id = feeds.user_id;
