d6850978791042f48a00408b08289e5953a95a29
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-08-23 01:20:13 +0200

    tidy main.go for clarity

--- main.go
+++ main.go
@@ -33,16 +33,19 @@ func main() {
 	}
 
 	cmds := commands{handlers: make(map[string]func(*state, command) error)}
-	cmds.register("addfeed", middlewareLoggedIn(handlerAddFeed))
+
 	cmds.register("agg", handlerAggregator)
 	cmds.register("feeds", handlerFeeds)
-	cmds.register("follow", middlewareLoggedIn(handlerFollow))
-	cmds.register("following", middlewareLoggedIn(handlerFollowing))
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
-	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 	cmds.register("users", handlerUsers)
+
+	// commands that need login authentication
+	cmds.register("addfeed", middlewareLoggedIn(handlerAddFeed))
+	cmds.register("follow", middlewareLoggedIn(handlerFollow))
+	cmds.register("following", middlewareLoggedIn(handlerFollowing))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 
 	cmd := command{}
 	cmd.name = args[1]
